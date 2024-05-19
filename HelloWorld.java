3e71721b9bfed11832376e5d961203815a7fdff9
Vaibhav Karad <80346370+vaibhav-karad@users.noreply.github.com>
2024-05-19 12:46:02 +0530

    Update HelloWorld.java
    1.2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
 
-  public static void main(String[] args) {
+  public static void sayHello() {
     System.out.println("Hello World!");
+  }
+
+  public static void main(String[] args) {
+    sayHello(); // Calling the sayHello method
   }
 }
